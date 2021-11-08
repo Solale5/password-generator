e9c48c7d837ad3c905447f5380b033bfe93e8e5c
Solale5 <solomon.alemu@sjsu.edu>
2021-11-07 21:49:27 -0800

    edits by thang

--- script.js
+++ script.js
@@ -9,10 +9,28 @@ function createPassword(Arr, Length) {
     return pwd;
 }
 
-function exampleGenerator(Length) {
-    var value = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,~,`,!,@,#,$,%,^,&,*,(,),-,+,|,_,=,\,[,],{,},<,>,?,/,.,;".split(",");
+function checkLength() {
+    var input = document.getElementById("length").value;
+    var goodLength = true;
+    if(input < 8) {
+        goodLength = false;
+    }
+    return goodLength;
 
-    var rand = createPassword(value, Length);
-    document.getElementById("generatedPwd").innerText = rand;
-    return rand;
+}
+
+function exampleGenerator(Length) {
+
+        if(checkLength() == false) {
+            var msg = "Too short"
+            document.getElementById("generatedPwd").innerText = msg;
+            return msg;
+        }
+        else {
+            var value = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,~,`,!,@,#,$,%,^,&,*,(,),-,+,|,_,=,\,[,],{,},<,>,?,/,.,;".split(",");
+
+            var rand = createPassword(value, Length);
+            document.getElementById("generatedPwd").innerText = rand;
+            return rand;
+        } 
 }
