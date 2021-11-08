0a41b1f9c5d4087409d147ce3cee61e8c1897fc2
Solale5 <solomon.alemu@sjsu.edu>
2021-11-07 18:52:45 -0800

    initial generator

---
+++ script.js
@@ -0,0 +1,18 @@
+function createPassword(Arr, Length) {
+    var arr = Arr;
+    var pwd = "";
+
+    for(var i = 0; i < Length; i++) {
+        pwd += arr[Math.floor(Math.random()*arr.length)];
+    }
+
+    return pwd;
+}
+
+function exampleGenerator(Length) {
+    var value = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,~,`,!,@,#,$,%,^,&,*,(,),-,+,|,_,=,\,[,],{,},<,>,?,/,.,;".split(",");
+
+    var rand = createPassword(value, Length);
+    document.getElementById("generatedPwd").innerText = rand;
+    return rand;
+}
